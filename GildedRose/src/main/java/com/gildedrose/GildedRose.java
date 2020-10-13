9f29bb51598f2b8629e6ddffe67e21cdc41267a5
Romain REMY <remy.romain@outlook.com>
2020-10-13 13:53:33 +0200

    RefactoringV1.2

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -11,12 +11,10 @@ class GildedRose {
 //Element conjuré
 
   void qualityofConjuredObject(int i) {
-    if (items[i].name.toLowerCase(Locale.FRANCE).contains("conjured")) {
-      if (items[i].quality > 0) {
-        items[i].quality = (items[i].quality) / 2;
-      } else {
-        items[i].quality = 0;
-      }
+    if (items[i].quality > 0) {
+    items[i].quality = (items[i].quality) / 2;
+    } else {
+    items[i].quality = 0;
     }
   }
 
@@ -51,10 +49,7 @@ class GildedRose {
   //Random object
 
   public void qualityofRandomObject(int i) {
-    if (!items[i].name.equals("Aged Brie")
-      && !items[i].name.toLowerCase(Locale.FRANCE).contains("conjured")
-      && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")
-      && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {  
       if (items[i].quality > 0) {
         items[i].quality = (items[i].quality) - 1;
       }
@@ -64,7 +59,7 @@ class GildedRose {
   //"Aged Brie" object
 
   public void qualityofAgedBrieObject(int i) {
-    if (items[i].name.equals("Aged Brie")) {
+    if(items[i].sellIn > 0) {
       items[i].quality = (items[i].quality) + 1;
     }
   }
@@ -72,7 +67,6 @@ class GildedRose {
   //"Backstage passes to a TAFKAL80ETC concert" object
 
   public void qualityofBACKObject(int i) {
-    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
       if (items[i].sellIn > 10) {
         items[i].quality = (items[i].quality) + 1;
       }
@@ -85,20 +79,34 @@ class GildedRose {
       if (items[i].sellIn <= 0) {
         items[i].quality = 0;
       }
-    }
+    
   }
   //"Sulfuras, Hand of Ragnaros" object ne bouge pas en qualité et jours restants
 
   public void updateQuality() {
     for (int i = 0; i < items.length; i++) {
-      if (items[i].sellIn > 0) {
-        qualityofRandomObject(i);
-        qualityofAgedBrieObject(i);
-        qualityofConjuredObject(i);
-      }
-      qualityofBACKObject(i);
+        switch (items[i].name) {
+            case "Aged Brie":
+            qualityofAgedBrieObject(i);
+            break;
+                
+            case "Backstage passes to a TAFKAL80ETC concert":
+            qualityofBACKObject(i);
+            break;
+
+            default:
+            if(items[i].sellIn > 0) {
+              if (items[i].name.toLowerCase(Locale.FRANCE).contains("conjured")) {
+                qualityofConjuredObject(i);    
+              } else {
+                qualityofRandomObject(i);
+              }
+              break;
+            }
+        }
       sellInofObject(i);
       qualityControl(i);
+        
     }
   }
 }
