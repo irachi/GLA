eb37b141fa0b56f5937860a99b87334bcca576d8
Romain REMY <remy.romain@outlook.com>
2020-10-22 09:10:58 +0200

    Refactoring du code GildedRose

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -4,85 +4,121 @@ import java.util.Locale;
  * @author ALI Irachi REMY Romain
  * @version 1.0
  */
-class GildedRose 
-{
-    Item[] items;
-    
-    public GildedRose(Item[] items) {
-        this.items = items;
+
+class GildedRose {
+  static int MINQUALITY;
+  static int MAXQUALITY = 50;
+  static String sulfuras = "Sulfuras, Hand of Ragnaros";
+  static int tenDaysOrBelow = 10;
+  Item[] items;
+  GildedRose(Item[] items) {
+    this.items = items;
+  }
+  /**
+   * @param i
+   */
+  void qualityofConjuredObject(int i) {
+    if (items[i].quality > 0) {
+      items[i].quality = (items[i].quality) / 2;
+    } else {
+      items[i].quality = 0;
     }
-
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) //Si le nom est différent des 2
-            {
-                if (items[i].quality > 0) // si la quality est positive
-                {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) // Si le nom est différént de celui la
-                    {
-                        items[i].quality = items[i].quality - 1; //décrémenter la quantité et fin
-                    }
-                }
-            } 
-            else { //Sinon
-                if (items[i].quality < 50) //Si la quantité est inférieur à 50
-                {
-                    items[i].quality = items[i].quality + 1; // on incrémente la qualité
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) // Si le nom est égal
-                    {
-                        if (items[i].sellIn < 11) // si la quantité vendu est inférieur à 11
-                        {
-                            if (items[i].quality < 50) // si la qualité est inférieur à 50
-                            {
-                                items[i].quality = items[i].quality + 1; // on incrémente on incrémente la qualité
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) // si la quantité est inférieur à 6
-                        {
-                            if (items[i].quality < 50) // si la qualité est égal à 50
-                            {
-                                items[i].quality = items[i].quality + 1; // on incrémente la qualité
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) 
-            {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) 
-            {
-                if (!items[i].name.equals("Aged Brie")) 
-                {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) 
-                    {
-                        if (items[i].quality > 0) 
-                        {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) 
-                            {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } 
-                    else 
-                    {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } 
-                else 
-                {
-                    if (items[i].quality < 50) 
-                    {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
+  }
+  /**
+   * @param i
+   */
+  public void qualityControl(int i) {
+    if (items[i].quality < MINQUALITY) {
+      items[i].quality = MINQUALITY;
     }
+    if (!items[i].name.equals(sulfuras)) {
+      if (items[i].quality > MAXQUALITY) {
+        items[i].quality = MAXQUALITY;
+      }
+    }
+  }
+  /**
+   * @param i
+   */
+  public void sellInControl(int i) {
+    if (items[i].sellIn < 0) {
+      items[i].sellIn = 0;
+      downQuality(i);
+    }
+  }
+  /**
+   * @param i
+   */
+  public void downQuality(int i) {
+    items[i].quality = (items[i].quality) / 2;
+  }
+  /**
+   * @param i
+   */
+  public void updateSellInofObject(int i) {
+    if (!items[i].name.equals(sulfuras)) {
+      items[i].sellIn = (items[i].sellIn) - 1;
+      sellInControl(i);
+    }
+  }
+  /**
+   * @param i
+   */
+
+  public void upQualityofRandomObject(int i) {
+    if (!items[i].name.equals(sulfuras)) {
+      if (items[i].quality > 0) {
+        items[i].quality = (items[i].quality) - 1;
+      }
+    }
+  }
+  /**
+   * @param i
+   */
+  public void upQualityofAgedBrieObject(int i) {
+    if (items[i].sellIn > 0) {
+      items[i].quality = (items[i].quality) + 1;
+    }
+  }
+  /**
+   * @param i
+   */
+  public void qualityofBACKObject(int i) {
+    if (items[i].sellIn > tenDaysOrBelow) {
+      items[i].quality = (items[i].quality) + 1;
+    }
+    if (items[i].sellIn >= 6 && items[i].sellIn <= tenDaysOrBelow) {
+      items[i].quality = (items[i].quality) + 2;
+    }
+    if (items[i].sellIn >= 1 && items[i].sellIn <= 5) {
+      items[i].quality = (items[i].quality) + 3;
+    }
+    if (items[i].sellIn <= 0) {
+      items[i].quality = 0;
+    }
+  }
+
+  public void updateQuality() {
+    for (int i = 0; i < items.length; i++) {
+      switch (items[i].name) {
+        case "Aged Brie":
+          upQualityofAgedBrieObject(i);
+          break;
+        case "Backstage passes to a TAFKAL80ETC concert":
+          qualityofBACKObject(i);
+          break;
+        default:
+          if (items[i].sellIn > 0) {
+            if (items[i].name.toLowerCase(Locale.FRANCE).contains("conjured")) {
+              qualityofConjuredObject(i);
+            } else {
+              upQualityofRandomObject(i);
+            }
+            break;
+          }
+      }
+      updateSellInofObject(i);
+      qualityControl(i);
+    }
+  }
 }
